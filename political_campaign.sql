7d27947503614cbe9403700e0145c1665bff042d
Pablo <pawel-blukacz@wp.pl>
2025-06-12 10:00:11 +0200

    inserting example rows

--- political_campaign.sql
+++ political_campaign.sql
@@ -30,7 +30,7 @@ CREATE TABLE business.events (
 
 CREATE TABLE business.commission (
 	commission_id SERIAL PRIMARY KEY,
-	commisstion_name VARCHAR(100) NOT NULL -- I made a typo that I noticed during inserting
+	commisstion_name VARCHAR(100) NOT NULL -- I made a typo that I noticed during inserting, fixed below
 );
 
 CREATE TABLE business.commission_person (
@@ -65,7 +65,7 @@ CREATE TABLE business.person (
 	gender VARCHAR(1),
 	phone_number VARCHAR(12) UNIQUE NOT NULL,
 	address_id INT NOT NULL,
-	CHECK (phone_number ~ '^\+\d{11}$'), 
+	CHECK (phone_number ~ '^\+\d{11}$'), -- polish phone number format "+48_________"
 	CHECK (gender IN ('M', 'F'))
 );
 
@@ -121,13 +121,13 @@ CREATE TABLE business.address (
 	house_number VARCHAR(15) NOT NULL,
 	flat_number VARCHAR(15),
 	postal_code VARCHAR(6) NOT NULL,
-	CHECK (postal_code ~ '^\d{2}-\d{3}$') -- same case as phone number - I know that not every postal code looks like this
+	CHECK (postal_code ~ '^\d{2}-\d{3}$') -- polish postal code format "__-___"
 );
-
 
 -- typo correction
 ALTER TABLE business.commission
-RENAME commisstion_name to commission_name; -- So I corrected it here so I don't have to CASCADE and define FKs again
+RENAME commisstion_name to commission_name;
+
 
 
 -- defining foreign keys, done separately because this is more readable for me 
@@ -177,6 +177,7 @@ ALTER TABLE business.donations
 ADD CONSTRAINT campaign_d_fk FOREIGN KEY (campaign_id) REFERENCES business.campaigns(campaign_id);
 
 
+
 -- adding record_ts to each table (a column that will record the time of insertion)
 ALTER TABLE business.costs
 ADD COLUMN record_ts DATE NOT NULL DEFAULT current_date;
@@ -222,3 +223,126 @@ ADD COLUMN record_ts DATE NOT NULL DEFAULT current_date;
 
 ALTER TABLE business.address
 ADD COLUMN record_ts DATE NOT NULL DEFAULT current_date;
+
+
+
+-- inserting rows
+INSERT INTO business.costs (item_description, price_per_unit)
+VALUES
+	('Campaign banners', 45.99),
+	('Paper stack', 5.99);
+
+INSERT INTO business.roles (role_name, role_description)
+VALUES
+	('Organizer', 'The person responsible for event organisation and managing problems related to the event.'),
+	('Representative', 'The person responsible for event representation in public media and marketing campaigns.');
+
+INSERT INTO business.election_groups (group_name, date_created)
+VALUES
+	('Huge democrats', '2012-03-13'),
+	('Small democrats', '2021-05-14');
+
+INSERT INTO business.address (city, street, house_number, flat_number, postal_code)
+VALUES
+	('Oslo', 'Torgatta', '16', NULL, '80-333'),
+	('Trondheim', 'Sandgata', '11a', '15', '75-567');
+
+INSERT INTO business.campaigns (campaign_name, start_date, end_date)
+VALUES
+	('We love dogs!', '2023-01-01 12:00:00', '2023-01-03 18:00:00'),
+	('We love cows!', '2022-12-01 08:00:00', '2022-12-31 22:00:00');
+
+INSERT INTO business.status (short_description, full_description)
+VALUES
+	('OK', 'The event is going as planned. No issues identified.'),
+	('Postponed', 'The start date of the event is delayed due to complications.');
+
+INSERT INTO business.person (first_name, last_name, gender, phone_number, address_id)
+VALUES 
+	('Pablo', 'Escobear', 'M', '+67987678654', (SELECT address_id FROM business.address 
+							WHERE UPPER(city) = 'OSLO' 
+							AND UPPER(street) = 'TORGATTA' 
+							AND house_number = '16'
+							AND postal_code = '80-333')),
+	('Claudia', 'Aidualc', 'F', '+38120175654', (SELECT address_id FROM business.address
+							WHERE UPPER(city) = 'TRONDHEIM' 
+							AND UPPER(street) = 'SANDGATA' 
+							AND UPPER(house_number) = '11A'
+							AND postal_code = '75-567'));
+
+INSERT INTO business.votes (person_id, group_id, vote_date)
+VALUES
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'PABLO' AND UPPER(last_name) = 'ESCOBEAR'),
+	 (SELECT group_id FROM business.election_groups WHERE UPPER(group_name) = 'HUGE DEMOCRATS'), '2022-09-21'),
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'CLAUDIA' AND UPPER(last_name) = 'AIDUALC'),
+	 (SELECT group_id FROM business.election_groups WHERE UPPER(group_name) = 'SMALL DEMOCRATS'), '2022-09-22');
+
+INSERT INTO business.donations (person_id, campaign_id, amount_donated, donation_date)
+VALUES
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'PABLO' AND UPPER(last_name) = 'ESCOBEAR'),
+	 (SELECT campaign_id FROM business.campaigns WHERE UPPER(campaign_name) = 'WE LOVE DOGS!'), 200.00, '2015-09-17'),
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'CLAUDIA' AND UPPER(last_name) = 'AIDUALC'),
+	 (SELECT campaign_id FROM business.campaigns WHERE UPPER(campaign_name) = 'WE LOVE COWS!'), 15000.00, '2017-09-21');
+
+INSERT INTO business.person_role (person_id, role_id, start_date, end_date)
+VALUES
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'PABLO' AND UPPER(last_name) = 'ESCOBEAR'),
+	 (SELECT role_id FROM business.roles WHERE UPPER(role_name) = 'REPRESENTATIVE'), '2022-01-01', '2022-01-03'),
+	((SELECT person_id FROM business.person WHERE UPPER(first_name) = 'CLAUDIA' AND UPPER(last_name) = 'AIDUALC'),
+	 (SELECT role_id FROM business.roles WHERE UPPER(role_name) = 'REPRESENTATIVE'), '2022-07-01', '2022-07-12');
+
+INSERT INTO business.commission (commission_name)
+VALUES ('Big commission'), ('Small commission');
+
+INSERT INTO business.voting_places (place_name, address_id)
+VALUES
+	('Huge School', (SELECT address_id FROM business.address 
+							WHERE UPPER(city) = 'OSLO' 
+							AND UPPER(street) = 'TORGATTA' 
+							AND house_number = '16'
+							AND postal_code = '80-333')),
+	('Small School', (SELECT address_id FROM business.address
+							WHERE UPPER(city) = 'TRONDHEIM' 
+							AND UPPER(street) = 'SANDGATA' 
+							AND UPPER(house_number) = '11A'
+							AND postal_code = '75-567'));
+
+INSERT INTO business.events (commission_id, place_id, event_name, event_description, start_date, end_date, status_id)
+VALUES
+	((SELECT commission_id FROM business.commission WHERE UPPER(commission_name) = 'BIG COMMISSION'),
+	 (SELECT place_id FROM business.voting_places WHERE UPPER(place_name) = 'HUGE SCHOOL'), 
+	 'King election',
+	 'The third national election of a king to rule the country', 
+	 '2023-09-17',
+	 '2023-09-21',
+	 (SELECT status_id FROM business.status WHERE UPPER(short_description) = 'OK')),
+	((SELECT commission_id FROM business.commission WHERE UPPER(commission_name) = 'SMALL COMMISSION'),
+	 (SELECT place_id FROM business.voting_places WHERE UPPER(place_name) = 'SMALL SCHOOL'), 
+	 'Queen election',
+	 'The third national election of a queen to rule the country alongside the king', 
+	 '2023-09-22',
+	 '2023-09-24',
+	 (SELECT status_id FROM business.status WHERE UPPER(short_description) = 'OK'));
+
+INSERT INTO business.costs_events (event_id, cost_item_id, quantity)
+VALUES
+	((SELECT event_id FROM business.events WHERE UPPER(event_name) = 'KING ELECTION'),
+	 (SELECT item_id FROM business.costs WHERE UPPER(item_description) = 'CAMPAIGN BANNERS'),
+	 55),
+	((SELECT event_id FROM business.events WHERE UPPER(event_name) = 'QUEEN ELECTION'),
+	 (SELECT item_id FROM business.costs WHERE UPPER(item_description) = 'PAPER STACK'),
+	 1000);
+
+INSERT INTO business.commission_person (commission_id, person_id, start_date, end_date)
+VALUES
+	((SELECT commission_id FROM business.commission WHERE UPPER(commission_name) = 'SMALL COMMISSION'),
+	 (SELECT person_id FROM business.person WHERE UPPER(first_name) = 'PABLO' AND UPPER(last_name) = 'ESCOBEAR'),
+	 '2023-09-21 08:00:00',
+	 '2023-09-21 22:00:00'),
+	((SELECT commission_id FROM business.commission WHERE UPPER(commission_name) = 'BIG COMMISSION'),
+	 (SELECT person_id FROM business.person WHERE UPPER(first_name) = 'CLAUDIA' AND UPPER(last_name) = 'AIDUALC'),
+	 '2023-09-23 08:00:00',
+	 '2023-09-25 22:00:00');
+
+-- record_ts works fine
+SELECT * FROM business.commission;
